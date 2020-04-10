2b6d268cbfdd48f0287c8b1d936f5b4d59ddecce
Reid Ashwill <reidashwill@gmail.com>
2020-04-10 11:26:22 -0700

    allow the order button to also display the order details, including size, ingredients, price, and delivery location

--- js/scripts.js
+++ js/scripts.js
@@ -14,21 +14,23 @@ PizzaShop.prototype.addPizza = function (pizza){
 }
   // business logic for pizza------------------------------------------------
 
-function Pizza(size, toppings, specialtyToppings, price){
-  this.size = size
-  this.toppings = toppings
-  this.specialtyToppings = specialtyToppings
-  this.price = 0
+function Pizza(size, toppings, specialtyToppings, cheese, deliveryLocation, price){
+  this.size = size;
+  this.toppings = toppings;
+  this.specialtyToppings = specialtyToppings;
+  this.cheeses = cheese;
+  this.deliveryLocation = deliveryLocation;
+  this.price = 0;
 }
 
 Pizza.prototype.pizzaPrice = function() {
-  if(this.size === "sm"){
+  if(this.size === "small"){
     this.price = 16
-  }else if(this.pize === "md"){
+  }else if(this.size === "medium"){
     this.price = 20
-  }else if(this.size === "lrg"){
+  }else if(this.size === "large"){
     this.price = 24
-  }else if(this.size === "xl"){
+  }else if(this.size === "extra large"){
     this.price = 28
   }
   return (this.price += (this.toppings.length / 2) + this.specialtyToppings.length)
@@ -38,9 +40,11 @@ Pizza.prototype.pizzaPrice = function() {
 $(document).ready(function(){
   $("form#pizza-builder").submit(function(event){
     event.preventDefault();
-    var inputtedSize = $("#size").val();
     var inputtedToppings = [];
     var inputtedSpecialtyToppings = [];
+    var inputtedCheese = [];
+    var inputtedSize = $("#size").val();
+    var inputtedDeliveryLocation = $("#delivery-location").val();
     $("input:checkbox[name=topping]:checked").each(function(){
       var topping = $(this).val();
       inputtedToppings.push(topping)
@@ -49,11 +53,15 @@ $(document).ready(function(){
       var specialtyTopping = $(this).val();
       inputtedSpecialtyToppings.push(specialtyTopping)
     })
+    $("input:checkbox[name=cheese]:checked").each(function(){
+      var cheese = $(this).val();
+      inputtedCheese.push(cheese)
+    })
   
    
-    var newPizza = new Pizza(inputtedSize, inputtedToppings, inputtedSpecialtyToppings)
+    var newPizza = new Pizza(inputtedSize, inputtedToppings, inputtedSpecialtyToppings, inputtedCheese, inputtedDeliveryLocation)
     console.log(newPizza.pizzaPrice())
     console.log(newPizza)
-    $("#return-field").append()
+    $("#return-field").append("We're firing up the replicators! Your " + newPizza.size + " pizza is on its way!  It will be fully loaded with:" + newPizza.toppings + "," + newPizza.specialtyToppings + "," + newPizza.cheeses + "!  Once we have verified your payment of " + newPizza.price + " strips of Gold Pressed Latinum, One of our Dabo Girls will be delivering it to " + newPizza.deliveryLocation + " as soon as it's ready!")
   })
 })
