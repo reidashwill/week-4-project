4ef3c3c4f45273da890f8ae4e6654523507eb2b7
Reid Ashwill <reidashwill@gmail.com>
2020-04-10 08:47:31 -0700

    add more business logic for Pizza object, including the beginning of price calculation

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+// Business logic for pizza shop
+
 function PizzaShop{
   this.pizzas = []
   this.currentId = 0;
@@ -9,25 +11,29 @@ PizzaShop.prototype.assignId = function(){
 PizzaShop.prototype.addPizza = function (pizza){
   contact.id = this.assignId();
   this.pizzas.push(pizza)
-}
-// business logic for pizza
+
+  // business logic for pizza
+
 function Pizza(){
   this.size = ""
   this.toppings = []
   this.specialtyToppings = []
+  this.price = 0
 }
-
+Pizza.prototype.pizzaSize = function(size) {
+  this.size.append(size)
+}
 Pizza.prototype.addTopping = function(topping) {
   this.toppings.push(topping)
 }
 Pizza.prototype.addSpecialtyTopping = function(specialtyToppings){
   this.specialtyToppings.push(specialtyTopping)
 }
+Pizza.prototype.pizzaPrice = function() {
+  this.price += this.toppings.length
+  }
 
-Pizza.prototype.showOrder = function()
 
-
-//Business logic for toppings
 
 
   
